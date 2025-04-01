c0319ffc7067ac0bd1872b87b32900be94d503e5
yazan-saideh <yazan.saideh11@gmail.com>
2025-04-01 21:44:30 +0300

    Delete game2 directory

--- game2/Assets/Script/Dialoug.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class Dialoug 
-{
-    [TextArea(3 , 10)]
-    public string[] sentences;
-}
